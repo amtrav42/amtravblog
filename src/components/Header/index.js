f9527a86b0942df593fd06aeb4b3deae861dbf81
Amelia Travers <amelia.travers@myob.com>
2021-06-11 12:18:45 +1000

    first cut refactor homepage css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
+import HeaderStyles from "./header.module.css";
 
 const Header = ({ isHomepage }) => {
   const homepageClass = isHomepage ? "homepage-header" : "";
--- src/components/HomepageBanner/index.js
+++ src/components/HomepageBanner/index.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "gatsby";
 import { RichText } from "prismic-reactjs";
+import HomepageBannerStyles from "./homepageBanner.module.css";
 
 const HomepageBanner = ({ bannerContent }) => (
   <section
-    className="homepage-banner"
+    className={HomepageBannerStyles.banner}
     style={{
       backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(${bannerContent.background.url})`,
     }}
   >
-    <div className="banner-content container">
-      <h2 className="banner-title">
+    <div className={HomepageBannerStyles.content}>
+      <h2 className={HomepageBannerStyles.title}>
         {RichText.asText(bannerContent.title.raw)}
       </h2>
-      <p className="banner-description">
+      <p className={HomepageBannerStyles.description}>
         {RichText.asText(bannerContent.description.raw)}
       </p>
-      <Link to={bannerContent.link.url} className="banner-button">
+      <Link to={bannerContent.link.url} className={HomepageBannerStyles.button}>
         {RichText.asText(bannerContent.linkLabel.raw)}
       </Link>
     </div>
